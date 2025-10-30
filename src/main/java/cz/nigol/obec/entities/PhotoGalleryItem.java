6c25276748afa0e27c7a2b70caede55e29e66942
Martin Polák <nigol@martin--mac-mini.local>
2025-10-30 17:27:49 +0100

    Entity class for photo gallery item.

---
+++ src/main/java/cz/nigol/obec/entities/PhotoGalleryItem.java
@@ -0,0 +1,85 @@
+package cz.nigol.obec.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "OB_PHOTOGALLERYITEM")
+public class PhotoGalleryItem implements Serializable {
+	private static final long serialVersionUID = -3262642900499913785L;
+
+	@Id
+	@Column(name = "ID")
+	private long id;
+
+	@Column(name = "LABEL", nullable = false, length = 1024)
+	private String label;
+
+	@Column(name = "URL", nullable = false, length = 1024)
+	private String url;
+
+	// Constructors
+	public PhotoGalleryItem() {
+	}
+
+	public PhotoGalleryItem(String label, String url) {
+		this.label = label;
+		this.url = url;
+	}
+
+	public PhotoGalleryItem(Long id, String label, String url) {
+		this.id = id;
+		this.label = label;
+		this.url = url;
+	}
+
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	// Optional: toString(), equals(), and hashCode()
+	@Override
+	public String toString() {
+		return "PhotoGalleryItem{" +
+				"id=" + id +
+				", label='" + label + '\'' +
+				", url='" + url + '\'' +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof PhotoGalleryItem)) return false;
+		PhotoGalleryItem that = (PhotoGalleryItem) o;
+		return id == that.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
+}
